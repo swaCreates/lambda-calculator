eec9fa3c080adff0a0fc339367f71b4635820082
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2019-12-20 21:23:34 -0500

    importing proper files in to designated files

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,6 +4,9 @@ const NumberButton = () => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button></button>
     </>
   );
 };
+
+export default NumberButton;
